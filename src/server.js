bbc0bc69616258736899e7dcc706ce000433e1b9
Ryan <ryan02ff@gmail.com>
2023-11-17 17:12:01 -0300

    adicionando cors para manuseio da aplicação com o frontend

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors")
+const cors = require("cors")
+
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 
@@ -11,8 +13,8 @@ migrationsRun()
 
 const app = express()
 app.use(express.json())
-
 app.use(routes)
+app.use(cors())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
